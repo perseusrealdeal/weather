268ac8fba40ec356d607879c6740fda45b8deb7b
Mikhail Zhigulin <50202963+perseusrealdeal@users.noreply.github.com>
2021-12-05 15:40:45 +0700

    Design network communication layer [OpenWeatherClient]
    This commit provides a ready-to-use solution that defines a set of statements to achieve weather data requesting via OpenWeather web service.
    
    Designed solution includes OpenWeatherClient class with weather parameter, onResultUpdate closure, and updateWeatherData method.
    
    weather parameter used to save the last update. It is empty since the instance created.
    
    onResultUpdate closure used to communicate update result as soon as weather data received.
    
    updateWeatherData method used to request weather data.
    
    // sample code
    
    llet network_client = OpenWeatherClient()
    network_client.onResultUpdate = { _ in  print("bla bla bla") }
    network_client.updateWeatherData()
    
    Together with the production code for the designed network layer, the full set of unit tests implemented with OpenWeatherCommunicationTests class is included into the solution.
    All tests require no network connection (offline mode) except the one test designed to check weather data requesting in online.

--- ios.native.weather/Weather/Business logic/Services/OpenWeatherClient.swift
+++ ios.native.weather/Weather/Business logic/Services/OpenWeatherClient.swift
@@ -7,74 +7,116 @@
 
 import Foundation
 
+// MARK: - Helper abstracts used to make code testable
+
+protocol URLSessionProtocol
+{
+    func dataTask(with request: URLRequest, completionHandler:
+                    @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession : URLSessionProtocol { }
+
+// MARK: - Failure scenario details
+
+enum RequestError : Error
+{
+    case invalidExclude
+    case invalidUrl
+    case failedRequest(String)
+}
+
+// MARK: - Client for communicating with OpenWeather Service
+
 class OpenWeatherClient
 {
-    private let OPENWEATHER_API_KEY = "79eefe16f6e4714470502074369fc77b"
+    private var api_key : String { "79eefe16f6e4714470502074369fc77b" }
     
     private var dataTask: URLSessionDataTask?
+    private let session : URLSessionProtocol // Isolated for unit testing
     
-    var onWeatherDataUpdate: (Data) -> Void = { print($0) }
+    var onResultUpdate  : (Result<Data, RequestError>) -> Void = { print($0) }
+    var weather         : Data { givenData }
+    
     private(set) var givenData: Data = Data()
     {
         didSet
         {
-            onWeatherDataUpdate(givenData)
+            onResultUpdate(.success(givenData))
         }
     }
     
-    func updateWeatherData(exclude: String = "hourly,minutely,daily",
-                           units: String = "metric",
-                           latitude: String = "55.662546456740564",
-                           longitude: String = "85.62138369331707")
+    // Constructor injection used to make URLSession isolated
+    init(session: URLSessionProtocol = URLSession.shared)
     {
-        guard let encodedExclude = exclude.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let url = URL(string:
-                                "https://api.openweathermap.org/data/2.5/onecall?" +
-                                "lat=\(latitude)" +
-                                "&lon=\(longitude)" +
-                                "&units=\(units)" +
-                                "&exclude=\(encodedExclude)" +
-                                "&appid=\(OPENWEATHER_API_KEY)")
-        else { return }
+        self.session = session
+    }
+    
+    func updateWeatherData(exclude  : String = "hourly,minutely,daily,alerts",
+                           latitude : String = "55.662546456740564",
+                           longitude: String = "85.62138369331707",
+                           units    : String = "metric")
+    {
+        // Validate parameters
+        
+        guard let encodedExclude
+                = exclude.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        else { onResultUpdate(.failure(.invalidExclude)); return }
+        
+        guard let url
+                = URL(string:
+                        "https://api.openweathermap.org/data/2.5/onecall?" +
+                        "lat=\(latitude)" +
+                        "&lon=\(longitude)" +
+                        "&units=\(units)" +
+                        "&exclude=\(encodedExclude)" +
+                        "&appid=\(api_key)")
+        else { onResultUpdate(.failure(.invalidUrl)); return }
         
         let request = URLRequest(url: url)
         
-        dataTask = URLSession.shared.dataTask(with: request)
-        { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+        // Send request
+        
+        dataTask = session.dataTask(with: request)
+        { [self] (weather: Data?, response: URLResponse?, error: Error?) -> Void in
             
-            var givenData: Data?
-            var errorMessage: String?
+            // Read answer
+            
+            var answer_data : Data?
+            var answer_error: String?
             
             if let error = error
             {
-                errorMessage = error.localizedDescription
+                answer_error = error.localizedDescription
             }
             else if let response = response as? HTTPURLResponse, response.statusCode != 200
             {
-                errorMessage = "Response: " + HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
+                answer_error = HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
             }
-            else if let data = data
+            else if let requested_data = weather
             {
-                givenData = data
+                answer_data = requested_data
             }
+            
+            // Communicate changes
             
             DispatchQueue.main.async
             { [weak self] in guard let self = self else { return }
                 
-                if let givenData = givenData
+                if let requested_data = answer_data
                 {
-                    self.givenData = givenData
+                    self.givenData = requested_data
                 }
-                if let errorMessage = errorMessage
+                else if let error = answer_error
                 {
-                    print(errorMessage)
+                    onResultUpdate(.failure(.failedRequest(error)))
                 }
                 
                 self.dataTask = nil
             }
-            
         }
         
         dataTask?.resume()
     }
 }
+
